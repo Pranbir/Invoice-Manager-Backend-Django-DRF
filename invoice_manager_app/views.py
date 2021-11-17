858cb8213429b8152062dae67ceb825de72ff490
ashabb <ashishgole99955@gmail.com>
2021-11-18 00:17:10 +0530

    moved

--- invoice_manager_app/views.py
+++ invoice_manager_app/views.py
@@ -2,18 +2,15 @@ from rest_framework import views, response,status,viewsets
 from . serializer import CustomerSerializer
 from django.http.response import JsonResponse
 from django.shortcuts import render
+from .models import Customer
+
 
 # Create your views here.
-
-#ashish
-
 def index(request):
     return JsonResponse({"msg" : "API is working"})
 
 
 # ashish
-from .models import Customer
-
 class CustomerAPI(viewsets.ModelViewSet):
     serializer_class=CustomerSerializer
     queryset=Customer.objects.all()
