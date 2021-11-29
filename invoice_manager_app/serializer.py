78399b9367bbe2605a4d9a6f2a6a05347280fe1d
desmondmiles11111 <68543904+abhishekdraut@users.noreply.github.com>
2021-11-29 22:18:30 +0530

    resolved

--- invoice_manager_app/serializer.py
+++ invoice_manager_app/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .import models as Allmodels
 from django.contrib.auth.models import User
 from .models import PaymentMode
 from .models import Tax
@@ -50,3 +51,78 @@ class TaxSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tax
         fields = '__all__'
+
+class customer_serializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    address = serializers.CharField(max_length=200, )
+    phone = serializers.CharField(max_length=20)
+    email = serializers.EmailField(max_length=100)
+
+
+class tax_serializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    rate = serializers.FloatField()
+
+
+class order_serializer(serializers.Serializer):
+
+    customer = serializers.IntegerField()
+    date = serializers.DateField()
+    due_date = serializers.DateField()
+    discount_type = serializers.CharField()
+    discount = serializers.FloatField()
+    paid_status = serializers.CharField()
+    total = serializers.FloatField()
+    due_amount = serializers.FloatField()
+
+
+class orderItem_serializer(serializers.Serializer):
+    order = serializers.IntegerField()
+    product=serializers.IntegerField()
+    quantity = serializers.IntegerField()
+    tax=serializers.IntegerField()
+    unit_price = serializers.FloatField()
+    total = serializers.FloatField()
+
+
+class invoice_transaction_serializer(serializers.Serializer):
+    
+    date = serializers.DateField()
+    amount = serializers.FloatField()
+    description = serializers.CharField()
+    payment_mode = serializers.IntegerField()
+    reference = serializers.CharField()
+
+#=====================model serializer class ==========================
+
+class order_modelserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Allmodels.Order
+        fields ='__all__'
+
+class customer_modelserializer(serializers.ModelSerializer):
+    orders=order_modelserializer(read_only=True,many=True)    #you have to defined in the primary table that you are going to give 
+    class Meta:
+        model=Allmodels.Customer
+        fields ='__all__'
+
+
+#=================invoice Transaction==================================
+
+class invoice_transaction_Modelserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Allmodels.InvoiceTransaction
+        fields ='__all__'
+
+class product_Modelserializer(serializers.ModelSerializer):
+    invoice=invoice_transaction_Modelserializer(read_only=True,many=True)
+    class Meta:
+        model=Allmodels.Product
+        fields ='__all__'
+
+class payment_mode_Modelserializer(serializers.ModelSerializer):
+    invoice=invoice_transaction_Modelserializer(read_only=True,many=True)
+    
+    class Meta:
+        model=Allmodels.PaymentMode
+        fields ='__all__'        
--- invoice_manager_app/urls.py
+++ invoice_manager_app/urls.py
@@ -19,6 +19,11 @@ router.register('product',views.ProductViewSet,basename='Product')
 router.register('paymentmode',views.PaymentModeAPI,basename='paymentmode')
 
 router.register('tax', views.TaxAPI, basename='tax')
+#Abhishek
+order_router=routers.DefaultRouter()
+order_router.register('orders',views.order_viewset,basename="order_viewset")
+invoice_router=routers.DefaultRouter()
+invoice_router.register('invoices',views.invoice)
 
 
 urlpatterns = [
@@ -26,5 +31,13 @@ urlpatterns = [
     path('',include(router.urls)),
     path('',include(Register_router.urls)),
     path('login',views.LoginView.as_view()),
-    path('logout',knox_views.LogoutView.as_view())
+    path('logout',knox_views.LogoutView.as_view()),
+    path('invoice/',views.invoice_details.as_view(),name='invoice_page'),
+    path("invoice/<int:invoice_id>",views.invoice_details.as_view(),name='invoice_page_with_id'),
+    path("orderItems/",views.order_item.as_view(),name='order_details'),
+    path("orderItems/<int:orderid>",views.order_item.as_view(),name='order_details_with_id'),
+    path("orders/<int:order>",views.orders.as_view(),name="orders_with_id"),
+    path("orders/",views.orders.as_view(),name="allorders"),
+    path("viewset_orders",include(order_router.urls)),
+    path("modelViewset_invoices",include(invoice_router.urls))
 ]
